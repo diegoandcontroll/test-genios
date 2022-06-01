b18a2c690e89dd849f36b7482486464d6bc4ef89
diegoandcontroll <lukasxdp2@gmail.com>
2022-06-01 14:47:02 -0300

    feat: create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-import {} from 'swiper/react';
 import { Cards } from './components/Cards';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Cards />
+      <Footer />
       <GlobalStyle />
     </>
   );

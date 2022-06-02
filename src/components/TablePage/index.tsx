8943950eac65a18fe848c6ca141a7bc8332ddf66
diegoandcontroll <lukasxdp2@gmail.com>
2022-06-02 15:45:58 -0300

    feat: create modal

--- src/components/TablePage/index.tsx
+++ src/components/TablePage/index.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-no-useless-fragment */
+import { useState } from 'react';
 import { GlobalStyle } from '../../styles/global';
+import { NewModal } from '../NewModal';
 import { Table } from '../Table';
 import { Container } from './styles';
 
 export function TablePage() {
+  const [modal, setModal] = useState(false);
+  function toggleModal() {
+    setModal(!modal);
+  }
   return (
     <>
       <Container>
@@ -16,12 +23,13 @@ export function TablePage() {
         </div>
         <div />
         <div>
-          <button>
+          <button onClick={toggleModal}>
             <span>+ Adicionar novo</span>
           </button>
         </div>
       </Container>
       <Table />
+      <NewModal isOpen={modal} onRequestClose={toggleModal} />
       <GlobalStyle />
     </>
   );
